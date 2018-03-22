11e1451284428f7651438e866a0ba9416c210e0b
xx <xiongxin0517@gmail.com>
2018-03-22 09:57:23 +0800

    a2

--- myHello.py
+++ myHello.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>欢迎光临, %s <h1>' % name
+    return '<h1>u"欢迎光临," %s <h1>' % name
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
