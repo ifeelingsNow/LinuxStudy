52257e1a3fde8e6bc123f39ec3c406205789b0b7
xx <xiongxin0517@gmail.com>
2018-03-23 17:25:15 +0800

    表单功能

--- myHello.py
+++ myHello.py
@@ -2,12 +2,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name=StringField(u'请输入您的名字', validators=[DataRequired()])
+    submit=SubmitField(u'提交')
 
 app = Flask(__name__)
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment=Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,12 +24,14 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'),500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # response = make_response('<h1>运行在了命令行上面!</h1>')
-    # response.set_cookie('answer', '42')
-    # return '<h1>新版本需要重新编译吗？看看中文字可不可以显示!</h1>', 400
-    return render_template('index.html', current_time=datetime.utcnow())
+    name=None
+    form=NameForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
