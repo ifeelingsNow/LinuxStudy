364f1a258df689f41c1c7f5f3b257b102ccb1d2e
xx <xiongxin0517@gmail.com>
2018-03-23 11:09:51 +0800

    架设服务器时，让服务器端可以使用命令行参数

--- myHello.py
+++ myHello.py
@@ -1,14 +1,14 @@
 ﻿# -*- coding: utf-8 -*-
 from flask import Flask
 from flask import make_response
-from flask.ext.script import Manager
+from flask_script import Manager
 
 app = Flask(__name__)
-
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
+    response = make_response('<h1>运行在了命令行上面!</h1>')
     response.set_cookie('answer', '42')
     # return '<h1>新版本需要重新编译吗？看看中文字可不可以显示!</h1>', 400
     return response
@@ -21,5 +21,5 @@ def user(name):
     return '<h1>欢迎光临,' + str(name) + '<h1>'
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
 
