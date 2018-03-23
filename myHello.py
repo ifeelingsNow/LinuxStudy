8088548c9ba73ac4de8b943812ad849e068aabce
xx <xiongxin0517@gmail.com>
2018-03-23 11:30:24 +0800

    使用模板

--- myHello.py
+++ myHello.py
@@ -1,25 +1,26 @@
 ﻿# -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 from flask import make_response
 from flask_script import Manager
 
 app = Flask(__name__)
-manager = Manager(app)
+# manager = Manager(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>运行在了命令行上面!</h1>')
-    response.set_cookie('answer', '42')
+    # response = make_response('<h1>运行在了命令行上面!</h1>')
+    # response.set_cookie('answer', '42')
     # return '<h1>新版本需要重新编译吗？看看中文字可不可以显示!</h1>', 400
-    return response
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
     # return '<h1>"欢迎光临," %s <h1>' % name
     s='look and see'
     b=type(name)
-    return '<h1>欢迎光临,' + str(name) + '<h1>'
+    return render_template('user.html', name=name)
+    # return '<h1>欢迎光临,' + str(name) + '<h1>'
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
 
