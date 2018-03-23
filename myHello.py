bb419922e9bfc0e8809133090352fbf02c18e232
xx <xiongxin0517@gmail.com>
2018-03-23 16:42:50 +0800

    添加资源

--- myHello.py
+++ myHello.py
@@ -1,10 +1,13 @@
 ﻿# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment=Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,7 +22,7 @@ def index():
     # response = make_response('<h1>运行在了命令行上面!</h1>')
     # response.set_cookie('answer', '42')
     # return '<h1>新版本需要重新编译吗？看看中文字可不可以显示!</h1>', 400
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
