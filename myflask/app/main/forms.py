703de565884ee1df5450d5ffc2ddc8d7c816ddc7
xx <xiongxin0517@gmail.com>
2018-04-03 17:27:56 +0800

    阿萨德飞洒

--- myflask/app/main/forms.py
+++ myflask/app/main/forms.py
@@ -1,7 +1,8 @@
+#-*- coding=utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
-    name = StringField('your name is?', validators=[DataRequired()])
-    submit = SubmitField('submit')
+    name = StringField(U'您的名字是?', validators=[DataRequired()])
+    submit = SubmitField(U'确定')
