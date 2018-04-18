2e372f1538f5348fed4b589075fce4cc515cbbe2
ifeelingsNow <xiongxin0517@gmail.com>
2018-04-18 21:46:59 +0800

    贷款计算

---
+++ FoundCal/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using StudyFunc;
+
+namespace KMP
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int pos = -1;
+            DateTime dt1 = DateTime.Now;
+            for (int i = 0; i < 100000; i++)
+            {
+                bool test = Func.simpleFind(c_oriStr.Text, c_findStr.Text, ref pos);
+            }
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = (dt2 - dt1);
+            MessageBox.Show(ts.Ticks.ToString());
+            Trace.WriteLine("1");
+        }
+
+        private void Button_Click_KMP(object sender, RoutedEventArgs e)
+        {
+            //测试用时
+            int pos = -1;
+            int[] Mlist = Func.KMPMatchList(c_findStr.Text);
+            DateTime dt1 = DateTime.Now;
+            for (int i = 0; i < 100000; i++)
+            {
+                bool test = Func.KMPFind(c_oriStr.Text, c_findStr.Text, Mlist, ref pos);
+            }
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = (dt2 - dt1);
+            MessageBox.Show(ts.Ticks.ToString());
+            Trace.WriteLine("1");
+        }
+    }
+}
