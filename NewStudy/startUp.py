01058c1951808649d1dab942e8154c3ca1abfe9a
xxin <2110391240@qq.com>
2018-12-17 14:39:20 +0800

    阿斯蒂芬

--- NewStudy/startUp.py
+++ NewStudy/startUp.py
@@ -8,8 +8,8 @@ import os
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'test.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-manager = Manager(app)
 db=SQLAlchemy(app)
+manager = Manager(app)
 
 
 referTable=db.Table('refertable',
