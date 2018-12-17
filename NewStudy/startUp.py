fb4058dcc81d439a33fff2c3a320f3caeb01368a
xxin <2110391240@qq.com>
2018-12-17 15:46:54 +0800

    终于可以提交了

--- NewStudy/startUp.py
+++ NewStudy/startUp.py
@@ -1,23 +1,31 @@
-from flask_script import Manager, Shell
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_moment import Moment
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
+from datetime import datetime
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'test.sqlite')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'test.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db=SQLAlchemy(app)
+
+moment = Moment(app)
+db = SQLAlchemy(app)
 manager = Manager(app)
 
-class Follow:
+class Follow(db.Model):
     __tablename__ = 'follows'
-    follower_id=db.Column(db.Integer, db.ForeignKey('users.id'))
-    followed_id=db.Column(db.Integer, db.ForeignKey('users.id'))
+    # 被哪些人关注
+    follower_id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    # 关注了的人
+    followed_id=db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-class User:
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String)
@@ -41,14 +49,27 @@ class User:
             except IntegrityError:
                 db.session.rollback()
 
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+    def unfollow(self,user):
+        f=self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+    def is_followed_by(self, user):
+        return self.follower.filter_by(follower_id=user.id).first() is not None
+
+
 def make_shell_context():
     return dict(app=app, db=db, Follow=Follow, User=User)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-@app.route('/')
-def begin():
-    return "first welcome!"
 
 if __name__ == '__main__':
     manager.run()
